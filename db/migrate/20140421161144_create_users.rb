2503a29d7ee2e76eec61a5660da3e6c764573614
App Academy Student <student@appacademy.io>
2014-04-22 16:22:17 -0400

    fixes commas

--- db/migrate/20140421161144_create_users.rb
+++ db/migrate/20140421161144_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, null: false,
-      t.string :email, null: false,
+      t.string :username, null: false
+      t.string :email, null: false
       t.string :password_digest, null: false
       t.string :token
 
