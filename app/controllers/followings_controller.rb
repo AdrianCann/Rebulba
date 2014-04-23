b77e47329acbb6e3361e5ca3b5a4d02e83950643
Adrian <adriantcann@gmail.com>
2014-04-23 08:06:06 -0400

    adds validation for following

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -8,16 +8,14 @@ class FollowingsController < ApplicationController
     params = following_params.merge(follower_id: current_user.id)
     @following = Following.new(params)
     if @following.save
-      redirect_to current_user
     else
-      flash.now[:errors] = ["No such person to follow!"]
+      flash.now[:errors] = @following.errors.full_messages
     end
-
+    redirect_to current_user
   end
 
   def destroy
     @following = Following.find_by_follower_id_and_followee_id(current_user.id, params[:id])
-
     @following.destroy
     redirect_to current_user
   end
--- app/models/following.rb
+++ app/models/following.rb
@@ -1,5 +1,7 @@
 class Following < ActiveRecord::Base
 
+  validate :cannot_follow_same_person_twice
+
   belongs_to :follower,
               class_name: "User",
               foreign_key: :follower_id,
@@ -9,5 +11,12 @@ class Following < ActiveRecord::Base
               class_name: "User",
               foreign_key: :followee_id,
               primary_key: :id
+
+  def cannot_follow_same_person_twice
+    count = Following.where("followee_id = ? AND follower_id = ?", self.followee_id, self.follower_id).count
+    if count > 0
+      errors.add(:followee_id, "You are already a follower!")
+    end
+  end
 
 end
