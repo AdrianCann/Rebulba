25a66e59e76d641ebce9c439dc365cf95bc65512
Adrian Cann <adriantcann@gmail.com>
2014-05-10 14:37:25 -0400

    about to mess with jbuilder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :require_logged_in, :only => [:new, :create]
+  before_action :require_logged_in
+  skip_before_action :require_logged_in, :only => [:new]
 
   def new
     @user = User.new
